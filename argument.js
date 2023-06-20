20a6b04254a83682d8c144b9b83fb35d8e33d4ee
Yasin Arafat Topu <69649959+Yasin-Topu@users.noreply.github.com>
2023-06-20 16:08:07 +0600

    argument

---
+++ argument.js
@@ -0,0 +1,11 @@
+function addNumber(num1,num2){
+    for(var i =0;i<arguments.length;i++){
+        var num = arguments[i];
+        var result = 0;
+        res = num + result;
+    }
+    return res;
+}
+
+var sum = addNumber(15,7,9,54);
+console.log(sum);
